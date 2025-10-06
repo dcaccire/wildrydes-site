486e038aa560e36160e84c73636d510a37957a8b
dcaccire <73723487+dcaccire@users.noreply.github.com>
2025-10-06 15:18:26 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,11 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_p9FJxWjn0', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '60v4ump8bs8o39kf68i5f3rflj', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
